e041e525119462fb4127f6f03dd7709966c5d70a
Scarlett <Scarlett@kayde.me>
2024-06-01 11:35:08 +0100

    build cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold">
       <div> :shrug: </div>
+      <div> Sign in </div>
     </nav>
   );
 }
