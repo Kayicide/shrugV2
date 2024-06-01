748ce678b632cde0523afb2c074ba173c017ef17
Scarlett <Scarlett@kayde.me>
2024-06-01 14:31:17 +0100

    temp moved into another component to stop errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,31 @@
 import { SignedIn } from "@clerk/nextjs";
-import Link from "next/link";
-import { db } from "~/server/db";
 import { getMyAudioFiles } from "~/server/queries";
 import { UploadButton } from "~/utils/uploadthing";
 
 //force dynamic for if I want to make sure the page isn't cached (it uses the db)
 export const dynamic = "auto";
 
+async function AudioFiles() {
+  const audioFiles = await getMyAudioFiles()
+  
+  return (
+    <div>
+      <UploadButton endpoint="audioUploader"></UploadButton>
+      {audioFiles.map((a) => 
+        <div key={a.id}>{a.name}</div>
+      )}
+    </div>
+  );
+}
+
 export default async function HomePage() {
-  const audioFiles = await getMyAudioFiles();
+  const audioFiles = await getMyAudioFiles()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
       <div className="container flex flex-col items-center justify-center">
         <SignedIn>
-          <UploadButton endpoint="audioUploader"></UploadButton>
-            {audioFiles.map((a) => 
-              <div key={a.id}>{a.name}</div>
-            )}
+          <AudioFiles />
         </SignedIn>
       </div>
     </main>
