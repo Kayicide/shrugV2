a59665fe5ff6fa105bc5283567edf4b0035ff8b0
Scarlett <Scarlett@kayde.me>
2024-06-01 12:11:52 +0100

    database stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
-  
+//force dynamic for if I want to make sure the page isn't cached (it uses the db)
+export const dynamic = "auto";
+
+export default async function HomePage() {
+  const audioFiles = await db.query.audio.findMany({
+    orderBy: (model, { desc }) => desc(model.id)
+  });
+
+  console.log(audioFiles);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center ">
       <div className="container flex flex-col items-center justify-center">
-        :shrug:
+        {audioFiles.map((a, index) => 
+          <div key={index}>{a.name}</div>
+        )}
       </div>
     </main>
   );
